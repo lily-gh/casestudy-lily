967c2b37be0245f79d6576397c1c9cbe57484ee8
Lily <8723107+lily-gh@users.noreply.github.com>
2023-11-28 20:50:20 +0100

    Initial commit (generated from start.spring.io)

---
+++ src/main/kotlin/com/lotto24/accountbalanceservice/AccountbalanceserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.lotto24.accountbalanceservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AccountbalanceserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<AccountbalanceserviceApplication>(*args)
+}
---
+++ src/test/kotlin/com/lotto24/accountbalanceservice/AccountbalanceserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lotto24.accountbalanceservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccountbalanceserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
